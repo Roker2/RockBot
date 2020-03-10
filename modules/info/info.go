ef87b5fdd3905a1af0c2c5850810d9d4ec01e880
Roker2 <dimaminko2000@gmail.com>
2020-03-10 22:09:52 +0300

    modules: info: Check LastName and Username

--- modules/info/info.go
+++ modules/info/info.go
@@ -11,7 +11,15 @@ func UserInfo(b ext.Bot, u *gotgbot.Update) error {
 	if u.Message.ReplyToMessage != nil {
 		user = u.Message.ReplyToMessage.From
 	}
-	textInfo := "<b>First Name:</b> " + user.FirstName + "\n<b>Last Name:</b> " + user.LastName + "\n<b>User ID:</b> <code>" + strconv.Itoa(user.Id) + "</code>\n<b>User Name:</b> @" + user.Username + "\n<b>Bot:</b> " + strconv.FormatBool(user.IsBot)
+	textInfo := "<b>First Name:</b> " + user.FirstName
+	if len(user.LastName) != 0 {
+		textInfo += "\n<b>Last Name:</b> " + user.LastName
+	}
+	textInfo += "\n<b>User ID:</b> <code>" + strconv.Itoa(user.Id)
+	if len(user.Username) != 0 {
+		textInfo += "</code>\n<b>User Name:</b> @" + user.Username
+	}
+	textInfo += "\n<b>Bot:</b> " + strconv.FormatBool(user.IsBot)
 	_, err := b.SendMessageHTML(u.Message.Chat.Id, textInfo)
 	return err
 }
