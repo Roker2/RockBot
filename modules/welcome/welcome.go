b1169cfd5a3b9b917519625674b9fdc72e1a1838
Roker2 <dimaminko2000@gmail.com>
2020-03-31 12:31:52 +0300

    welcome: Fix typo

--- modules/welcome/welcome.go
+++ modules/welcome/welcome.go
@@ -62,6 +62,6 @@ func SetWelcome(b ext.Bot, u *gotgbot.Update, args []string) error {
 	if err != nil {
 		return err
 	}
-	_, err = b.SendMessageHTML(u.Message.Chat.Id, "Новое приветствие установлено./n" + welcome)
+	_, err = b.SendMessageHTML(u.Message.Chat.Id, "Новое приветствие установлено.\n" + welcome)
 	return err
 }
