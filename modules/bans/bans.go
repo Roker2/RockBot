1b7edf4baefcb9096dd2e110fa3a812640aeb935
Roker2 <dimaminko2000@gmail.com>
2020-05-08 19:00:07 +0300

    modules: bans: Unban: Some fixes

--- modules/bans/bans.go
+++ modules/bans/bans.go
@@ -59,51 +59,48 @@ func Ban(b ext.Bot, u *gotgbot.Update, args []string) error {
 }
 
 func Unban(b ext.Bot, u *gotgbot.Update, args []string) error {
-	chat := u.Message.Chat
 	banId, errortext := utils.ExtractId(b, u, args)
 	if banId == 0 {
-		_, err := b.SendMessage(chat.Id, errortext)
+		_, err := b.SendMessage(u.Message.Chat.Id, errortext)
 		return err
 	}
 	if utils.ItIsMe(b, u, banId) {
 		return nil
 	}
 	logrus.Println(strconv.Itoa(banId))
-	if utils.IsUserInChat(chat, banId) {
-		_, err := b.SendMessage(chat.Id, texts.UserIsInTheChat)
+	if utils.IsUserInChat(u.Message.Chat, banId) {
+		_, err := b.SendMessage(u.Message.Chat.Id, texts.UserIsInTheChat)
 		return err
 	}
-	member, err := chat.GetMember(u.Message.From.Id)
+	member, err := u.Message.Chat.GetMember(u.Message.From.Id)
 	if !utils.BotIsAdministrator(b, u) {
 		return err
 	}
-	banMember, err := chat.GetMember(banId)
+	banMember, err := u.Message.Chat.GetMember(banId)
 	if err != nil {
 		return err
 	}
 	if !utils.MemberIsAdministrator(member) {
-		_, err = b.SendMessage(chat.Id, texts.YouAreNotAdministrator)
+		_, err = b.SendMessage(u.Message.Chat.Id, texts.YouAreNotAdministrator)
 		return err
 	}
-	if !member.CanRestrictMembers && !utils.MemberIsCreator(member) {
+	if !utils.MemberCanRestrictMembers(b, u) {
 		_, err = b.SendMessage(u.Message.Chat.Id, texts.YouCanNotToDoSomethingWithUsers)
 		return err
 	}
-	utils.MemberCanRestrictMembers(b, u)
 	if utils.MemberIsAdministrator(banMember) {
-		_, err = b.SendMessage(chat.Id, texts.ICanNotToBanAdministrator)
+		_, err = b.SendMessage(u.Message.Chat.Id, texts.ICanNotToBanAdministrator)
 		return err
-	} else {
-		_, err = chat.UnbanMember(banId)
-		if err != nil {
-			return err
-		}
-		err = sql.ResetUserWarns(u.Message.Chat.Id, banId)
-		if err != nil {
-			return err
-		}
-		_, err = b.SendMessage(chat.Id, texts.UserIsUnbanned(banMember.User.FirstName))
 	}
+	_, err = u.Message.Chat.UnbanMember(banId)
+	if err != nil {
+		return err
+	}
+	err = sql.ResetUserWarns(u.Message.Chat.Id, banId)
+	if err != nil {
+		return err
+	}
+	_, err = b.SendMessage(u.Message.Chat.Id, texts.UserIsUnbanned(banMember.User.FirstName))
 	return err
 }
 
