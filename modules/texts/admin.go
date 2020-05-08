57919a48b923ab5e1b1f83a5bdcd3cf0baa62c11
Roker2 <dimaminko2000@gmail.com>
2020-05-08 18:02:57 +0300

    modules: texts: admin: Init

---
+++ modules/texts/admin.go
@@ -0,0 +1,13 @@
+package texts
+
+const PleaseReplyToTheMessageYouWantToPin = "Ответьте пожалуйста на сообщение, которое Вы хотите закрепить."
+
+const ThisChatIsPrivateICanNotToPinMessage = "Данный чат приватный, в приватных чатах я не могу закрепить сообщение."
+
+const ThisChatIsPrivateICanNotToUnpinMessage = "Данный чат приватный, в приватных чатах я не могу открепить сообщение."
+
+const PleaseReplyToTheMessageOfThePersonYouWantToGrantAdministratorRightsToOrEnterTheirID = "Ответьте пожалуйста на сообщение того, кому Вы хотите выдать права администратора, или введите его ID."
+
+const PleaseReplyToTheMessageOfThePersonYouWantToRemoveAdministratorRightsToOrEnterTheirID = "Ответьте пожалуйста на сообщение того, кому Вы хотите убрать права администратора, или введите его ID."
+
+const PurgeCompleted = "Очистка завершена. Сообщение удалится через 5 секунд."
