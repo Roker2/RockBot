2c42ef3fa9a94002322984052b84e2a79ec9b9db
Roker2 <dimaminko2000@gmail.com>
2020-05-18 16:51:03 +0300

    modules: texts: warns: Init

---
+++ modules/texts/warns.go
@@ -0,0 +1,21 @@
+package texts
+
+import "strconv"
+
+const ICanNotGiveWarnToAdministrator = "Я не могу дать предупреждение администратору."
+
+func WarnsQuantityOfUser(FirstName string, quantity int, maxQuantity int) string {
+	return "Количество предупреждений у " +FirstName + ": " + strconv.Itoa(quantity) + "/" + strconv.Itoa(maxQuantity)
+}
+
+func NewWarnsQuantity(quantity string) string {
+	return "Новое количество максимальных предупреждений: " + quantity + "."
+}
+
+const WritePleaseInteger = "Введите пожалуйста число."
+
+const AdministratorAlwaysIsClean = "У администраторов всегда карма чистая. По крайней мере здесь."
+
+func UserDoesNotHaveWarns(FirstName string) string {
+	return "У пользователя " + FirstName + " очищена карма."
+}
