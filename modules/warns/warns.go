a41680fc6be15311eaab86a089d6b204246a8927
Roker2 <dimaminko2000@gmail.com>
2020-05-18 16:51:39 +0300

    modules: warns: Small fix and use new texts consts

--- modules/warns/warns.go
+++ modules/warns/warns.go
@@ -22,7 +22,7 @@ func WarnUser(b ext.Bot, u *gotgbot.Update, args []string) error {
  		return err
 	}
  	if utils.MemberIsAdministrator(banMember) {
- 		_, err = b.SendMessage(u.Message.Chat.Id, "Я не могу дать предупреждение администратору.")
+ 		_, err = b.SendMessage(u.Message.Chat.Id, texts.ICanNotGiveWarnToAdministrator)
  		return err
  	}
 	maxQuantity, err := sql.GetWarnsQuantityOfChat(u.Message.Chat.Id)
@@ -33,7 +33,7 @@ func WarnUser(b ext.Bot, u *gotgbot.Update, args []string) error {
   	if err != nil {
     	return err
   	}
-  	_, err = b.SendMessage(u.Message.Chat.Id, "Количество предупреждений у " + banMember.User.FirstName + ": " + strconv.Itoa(quantity) + "/" + strconv.Itoa(maxQuantity))
+  	_, err = b.SendMessage(u.Message.Chat.Id, texts.WarnsQuantityOfUser(banMember.User.FirstName, quantity, maxQuantity))
   	if err != nil {
   		return err
   	}
@@ -70,7 +70,7 @@ func GetUserWarns(b ext.Bot, u *gotgbot.Update, args []string) error {
 		if err != nil {
 			return err
 		}
-    	_, err = b.SendMessage(u.Message.Chat.Id, "Количество предупреждений у " + banMember.User.FirstName + ": " + strconv.Itoa(UserWarns) + "/" + strconv.Itoa(maxQuantity))
+    	_, err = b.SendMessage(u.Message.Chat.Id, texts.WarnsQuantityOfUser(banMember.User.FirstName, UserWarns, maxQuantity))
     	if err != nil {
     		return err
     	}
@@ -90,11 +90,15 @@ func SetWarnsQuantity (b ext.Bot, u *gotgbot.Update, args []string) error {
 		_, err = b.SendMessage(u.Message.Chat.Id, texts.YouAreNotAdministrator)
 		return err
 	}
+	if !utils.MemberCanRestrictMembers(b, u) {
+		_, err = b.SendMessage(u.Message.Chat.Id, texts.YouCanNotDoSomethingWithUsers)
+		return err
+	}
 	//var quantity int
   	if len(args) >= 1 {
     	quantity, err := strconv.Atoi(args[0])
     	if err != nil {
-    		_, err = b.SendMessage(u.Message.Chat.Id, "Введите пожалуйста число, а не бред.")
+    		_, err = b.SendMessage(u.Message.Chat.Id, texts.WriteIdNotBadText)
  			if err != nil {
  				return err
  			}
@@ -103,12 +107,12 @@ func SetWarnsQuantity (b ext.Bot, u *gotgbot.Update, args []string) error {
 		if err != nil {
 			return err
 		}
-		_, err = b.SendMessage(u.Message.Chat.Id, "Новое количество максимальных предупреждений: " + args[0] + ".")
+		_, err = b.SendMessage(u.Message.Chat.Id, texts.NewWarnsQuantity(args[0]))
 		if err != nil {
 			return err
 		}
   	} else {
-    	_, err := b.SendMessage(u.Message.Chat.Id, "Введите пожалуйста число.")
+    	_, err := b.SendMessage(u.Message.Chat.Id, texts.WritePleaseInteger)
     	if err != nil {
       return err
     }
@@ -117,50 +121,24 @@ func SetWarnsQuantity (b ext.Bot, u *gotgbot.Update, args []string) error {
 }
 
 func ResetWarns (b ext.Bot, u *gotgbot.Update, args []string) error {
-	if !utils.BotIsAdministrator(b, u) {
-		return nil
+	canBan, banId, err := utils.CommonBan(b, u, args)
+	if !canBan {
+		return err
 	}
-	var banId int
-  	if len(args) >= 1  {
-    	banId2, err := strconv.Atoi(args[0])
-    	if err != nil {
-      		_, err = b.SendMessage(u.Message.Chat.Id, "Введите пожалуйста ID, а не бред.")
-      		if err != nil {
-        		return err
-      		}
-    	}
-    	banId = banId2
-  	} else {
-    	if !utils.IsReply(b, u, true) {
-      		return nil
-    	}
-    	banId = u.Message.ReplyToMessage.From.Id
-  	}
-  	if utils.ItIsMe(b, u, banId) {
-    	return nil
-  	}
-  	log.Print(strconv.Itoa(banId))
-  	member, err := u.Message.Chat.GetMember(u.Message.From.Id)
-  	if !utils.BotIsAdministrator(b, u) {
-    	return err
-  	}
-  	if !utils.MemberIsAdministrator(member) {
-    	_, err = b.SendMessage(u.Message.Chat.Id, texts.YouAreNotAdministrator)
-    	return err
-  	}
-  	log.Print(strconv.Itoa(banId))
-  	utils.MemberCanRestrictMembers(b, u)
   	banMember, err := u.Message.Chat.GetMember(banId)
+  	if err != nil {
+  		return err
+	}
   	if banMember != nil {
     	if utils.MemberIsAdministrator(banMember) {
-      	_, err = b.SendMessage(u.Message.Chat.Id, "У администраторов всегда карма чистая. По крайней мере здесь.")
-      	return err
+			_, err = b.SendMessage(u.Message.Chat.Id, texts.AdministratorAlwaysIsClean)
+			return err
     	}
   	}
   	err = sql.ResetUserWarns(u.Message.Chat.Id, banId)
   	if err != nil {
     return err
   }
-  	_, err = b.SendMessage(u.Message.Chat.Id, "У пользователя " + banMember.User.FirstName + " очищена карма.")
+  	_, err = b.SendMessage(u.Message.Chat.Id, texts.UserDoesNotHaveWarns(banMember.User.FirstName))
   	return err
 }
