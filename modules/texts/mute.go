3eba65a03e47b2bd2685939518c92a34ff0a61b2
Roker2 <dimaminko2000@gmail.com>
2020-05-28 00:04:32 +0300

    modules: texts: mute: ThisCommandForTemporaryMute: Fix typo

--- modules/texts/mute.go
+++ modules/texts/mute.go
@@ -2,7 +2,7 @@ package texts
 
 const TalkToMeHere = "Поговори мне тут..."
 
-const ThisCommandForTemporaryMute = "Данная команда предназначена для временного mute.\nm - минуты, h - часы, d - дни.\nПример использования: /mute @user 1h 30m"
+const ThisCommandForTemporaryMute = "Данная команда предназначена для временного mute.\nm - минуты, h - часы, d - дни.\nПример использования: /tmute @user 1h 30m"
 
 func UserIsMuted(name string) string {
 	return "Пользователь " + name + " теперь будет молчать."
