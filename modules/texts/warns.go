bca04603e74a32623a883289263646dc482b24d3
Roker2 <dimaminko2000@gmail.com>
2020-09-16 17:57:03 +0300

    modules: texts: warns: Refactor

--- modules/texts/warns.go
+++ modules/texts/warns.go
@@ -4,22 +4,22 @@ import "strconv"
 
 const ICanNotGiveWarnToAdministrator = "Я не могу дать предупреждение администратору."
 
+const WritePleaseInteger = "Введите пожалуйста число."
+
+const AdministratorAlwaysIsClean = "У администраторов всегда карма чистая. По крайней мере здесь."
+
+const RemoveWarn = "Убрать предупреждение"
+
+const WarnWasRemoved = "Предупреждение убрано."
+
 func WarnsQuantityOfUser(FirstName string, quantity int, maxQuantity int) string {
-	return "Количество предупреждений у " +FirstName + ": " + strconv.Itoa(quantity) + "/" + strconv.Itoa(maxQuantity)
+	return "Количество предупреждений у " + FirstName + ": " + strconv.Itoa(quantity) + "/" + strconv.Itoa(maxQuantity)
 }
 
 func NewWarnsQuantity(quantity string) string {
 	return "Новое количество максимальных предупреждений: " + quantity + "."
 }
 
-const WritePleaseInteger = "Введите пожалуйста число."
-
-const AdministratorAlwaysIsClean = "У администраторов всегда карма чистая. По крайней мере здесь."
-
 func UserDoesNotHaveWarns(FirstName string) string {
 	return "У пользователя " + FirstName + " очищена карма."
 }
-
-const RemoveWarn = "Убрать предупреждение"
-
-const WarnWasRemoved = "Предупреждение убрано."
