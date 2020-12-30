9b113b6cce713245eb5fbc87fc3b067351240222
Roker2 <dimaminko2000@gmail.com>
2020-12-30 14:27:46 +0300

    modules: texts: rules: Use fmt

--- modules/texts/rules.go
+++ modules/texts/rules.go
@@ -1,9 +1,11 @@
 package texts
 
+import "fmt"
+
 const DescriptionOfRulesCommand = "Эта комманда позволяет установить правила."
 
 const NewRulesWereAdded = "Правила установлены! Вы можете посмотреть их с помощью команды /rules."
 
 func ErrorOfGettingRules(errText string) string {
-	return "Ошибка получения правил.\n" + errText
+	return fmt.Sprintf("Ошибка получения правил.\n%s", errText)
 }
