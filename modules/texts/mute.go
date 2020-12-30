3306749dda577d37b331bf44dc9fa0dd71ff64c0
Roker2 <dimaminko2000@gmail.com>
2020-12-30 14:26:06 +0300

    modules: texts: mute: Use fmt

--- modules/texts/mute.go
+++ modules/texts/mute.go
@@ -1,10 +1,12 @@
 package texts
 
+import "fmt"
+
 const TalkToMeHere = "Поговори мне тут..."
 
 const ThisCommandForTemporaryMute = "Данная команда предназначена для временного mute.\n" +
 	"m - минуты, h - часы, d - дни.\nПример использования: /tmute @user 1h 30m"
 
 func UserIsMuted(name string) string {
-	return "Пользователь " + name + " теперь будет молчать."
+	return fmt.Sprintf("Пользователь %s теперь будет молчать.", name)
 }
